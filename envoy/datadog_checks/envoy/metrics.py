b06bfe07cee8ab3b0bcc2f9cb604af65694e1c2b
Ofek Lev <ofekmeister@gmail.com>
2018-02-26 14:46:10 -0500

    add runtime metrics

--- envoy/datadog_checks/envoy/metrics.py
+++ envoy/datadog_checks/envoy/metrics.py
@@ -1,6 +1,26 @@
 from .utils import make_metric_tree
 
 METRICS = {
+    'runtime.load_error': {
+        'tags': (),
+        'method': 'count',
+    },
+    'runtime.override_dir_not_exists': {
+        'tags': (),
+        'method': 'count',
+    },
+    'runtime.override_dir_exists': {
+        'tags': (),
+        'method': 'count',
+    },
+    'runtime.load_success': {
+        'tags': (),
+        'method': 'count',
+    },
+    'runtime.num_keys': {
+        'tags': (),
+        'method': 'gauge',
+    },
     '': {
         'tags': (),
         'method': '',
